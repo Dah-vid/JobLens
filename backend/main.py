0ba0c62fb73ed195b5118c6ccb87d9c2adfe2abc
Dah-vid <davidtopair@gmail.com>
2026-03-26 07:56:51 +0000

    add POST/jobs endpoint with pydanctic validation

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,16 @@
-from pydantic import Basemodel
+from pydantic import BaseModel
 from fastapi import FastAPI
 from dotenv import load_dotenv
 load_dotenv()
 app = FastAPI()
 
+class JobInput(BaseModel):
+    description: str
+
 @app.get("/")
 def health_check():
     return {"status": "we live"}
+
+@app.post("/jobs")
+def create_job(job: JobInput):
+    return {"received": job}
